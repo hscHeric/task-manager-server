fe9eeb0343904070a9e49fb1bc8dadc16064b970
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-09-03 12:24:47 -0300

    mudando atribuito id da struct task

---
+++ internal/task/task.go
@@ -0,0 +1,25 @@
+package task
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Task representa uma tarefa com todos os detalhes.
+type Task struct {
+	TaskID      string     `json:"taskId"`         // Identificador único da tarefa
+	Title       string     `json:"title"`          // Título da tarefa
+	Description string     `json:"description"`    // Descrição da tarefa
+	Date        *time.Time `json:"date,omitempty"` // Data da tarefa, opcional
+}
+
+// NewTask cria uma nova instância de Task com os parâmetros fornecidos.
+func NewTask(title string, description string, date *time.Time) *Task {
+	return &Task{
+		TaskID:      uuid.NewString(), // Gera um novo UUID para a tarefa
+		Title:       title,
+		Description: description,
+		Date:        date,
+	}
+}
