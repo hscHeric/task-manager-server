24b8cae10d59f23588d314de81f31b59b1946778
Gabriel Almeida Sousa <110002760+Almeida-S-Gabriel@users.noreply.github.com>
2024-09-14 14:10:25 -0300

    Update udpServer.go

--- internal/udpServer/udpServer.go
+++ internal/udpServer/udpServer.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"sync"
 
+	"github.com/hscHeric/task-manager-server/internal/db"
 	"github.com/hscHeric/task-manager-server/internal/message"
 )
 
@@ -90,3 +91,61 @@ func (s *UDPServer) SendResponse(addr *net.UDPAddr, response *message.Message) e
 func (s *UDPServer) Close() error {
 	return s.conn.Close()
 }
+
+func (s *UDPServer) handleRequest(m *message.Message) []byte {
+	skeleton := db.NewSkeleton()
+	dispatcher := db.NewDispacher(skeleton)
+	response, err := dispatcher.Invoke(m.ObjReference, m.MethodID, m.Args)
+
+	if err != nil {
+		log.Printf("Erro ao obter resposta: %v", err)
+		return nil
+	}
+
+	return response
+}
+
+func (s *UDPServer) Start() {
+	log.Println("Servidor UDP iniciado e aguardando mensagens...")
+
+	for {
+		msg, addr, err := s.GetRequest()
+		if err != nil {
+			log.Printf("Erro ao receber mensagem: %v", err)
+			continue // Pula para a próxima iteração caso ocorra um erro
+		}
+
+		log.Printf("Mensagem recebida do cliente %s: %+v", addr.String(), msg)
+		response := s.handleRequest(msg)
+
+		if response == nil {
+			// Se a resposta for nil, envie uma mensagem de encerramento
+			shutdownMessage := &message.Message{
+
+				ObjReference: msg.ObjReference,
+				MethodID:     msg.MethodID,
+				Args:         nil,
+				T:            1,
+				ID:           msg.ID,
+				StatusCode:   500,
+			}
+
+			err = s.SendResponse(addr, shutdownMessage)
+			if err != nil {
+				log.Printf("Erro ao enviar mensagem de encerramento: %v", err)
+			}
+			log.Println("Encerrando o servidor devido a resposta nil.")
+			break // Encerra o loop e o servidor
+		}
+
+		msg.Args = response
+		msg.T = 1
+		msg.StatusCode = 200
+		err = s.SendResponse(addr, msg)
+		if err != nil {
+			log.Printf("Erro ao enviar resposta: %v", err)
+		}
+	}
+
+}
+
